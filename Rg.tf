c64119fbeffd18b03e01f066e7bbf8db1d5d27ad
Vijay Mishra <vijaysony328@gmail.com>
2026-08-16 02:25:48 +0530

    Fix formatting of variable declaration and for_each

--- Rg.tf
+++ Rg.tf
@@ -1,9 +1,9 @@
 
-variable "resource_names"{}
+variable "resource_names" {}
 
 resource "azurerm_resource_group" "rg_block" {
 
-for_each = var.resource_names
+  for_each = var.resource_names
 
   name     = each.value.name
   location = each.value.location
