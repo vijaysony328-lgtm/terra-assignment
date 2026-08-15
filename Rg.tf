06dfc7bd21b13fc0e950ad2c111d6f314fa4e934
vijaysony328-lgtm <vijaysony328@gmail.com>
2026-08-16 00:13:46 +0530

    add new files

---
+++ Rg.tf
@@ -0,0 +1,10 @@
+
+variable "resource_names"{}
+
+resource "azurerm_resource_group" "rg_block" {
+
+for_each = var.resource_names
+
+  name     = each.value.name
+  location = each.value.location
+}
---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+
+resource_names = {
+    rg1={
+
+        name = "rg1_kit"
+        location = "East US"
+    }
+}
