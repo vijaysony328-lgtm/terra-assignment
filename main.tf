3cf1dbca0a67f23f4b299e3a0178ea8106063592
Vijay Mishra <vijaysony328@gmail.com>
2026-08-16 02:26:47 +0530

    Fix formatting in main.tf for provider block

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = ">=4.0.0"
-      
+      source  = "hashicorp/azurerm"
+      version = ">=4.0.0"
+
+    }
   }
-  }
-  }
+}
 
 
 provider "azurerm" {
